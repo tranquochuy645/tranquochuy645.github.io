849c41a47028729f8f7d2d6a93e19328cecd9ced
huy <tranquochuy645@gmail.com>
2023-06-08 23:20:03 +0700

    o

--- src/components/Main/Landing/Landing.tsx
+++ src/components/Main/Landing/Landing.tsx
@@ -6,8 +6,16 @@ interface TypeTextOptions {
     delay: number;
 }
 function Landing() {
-    const text_1 = "Hi, I'm ";
-    const text_2 = "Huy";
+    const arrT = [
+        "Hi, I'm ",
+        "Huy",
+        "I'm currently studying Embedded Systems and IoT at ",
+        "HCMC University of Technology and Education",
+        "With my graduation anticipated by June 2024, my primary aspiration is to become a ",
+        "full-stack developer"
+    ]
+
+
     function typeText({ element, text, delay }: TypeTextOptions, callback?: () => void): void {
         let index = 0;
         const textContainer = document.createElement('span');
@@ -32,19 +40,42 @@ function Landing() {
 
 
     useEffect(() => {
-        const el_1 = document.querySelector('#landing>.container:first-child>span:last-child h1 span:first-child');
-        const el_2 = document.querySelector('#landing>.container:first-child>span:last-child h1 span:last-child');
-        if (el_1 && el_2) {
+        const el_wrapper = document.querySelector('#landing>.about>span:last-of-type');
+        const el_1 = el_wrapper?.querySelector('h1>span:first-of-type');
+        const el_2 = el_wrapper?.querySelector('h1>span:last-of-type');
+        const el_3 = el_wrapper?.querySelector('p:first-of-type>span:first-of-type');
+        const el_4 = el_wrapper?.querySelector('p:first-of-type>a:first-of-type');
+        const el_5 = el_wrapper?.querySelector('p:last-of-type>span:first-of-type');
+        const el_6 = el_wrapper?.querySelector('p:last-of-type>span:last-of-type');
+        if (el_1 && el_2 && el_3 && el_4 && el_5 && el_6) {
             setTimeout(() => {
                 typeText(
-                    {
-                        element: el_1 as HTMLElement,
-                        text: text_1,
-                        delay: 200
-
-                    },
+                    { element: el_1 as HTMLElement, text: arrT[0], delay: 200 },
                     () => {
-                        typeText({ element: el_2 as HTMLElement, text: text_2, delay: 200 })
+                        typeText(
+                            { element: el_2 as HTMLElement, text: arrT[1], delay: 200 },
+                            () => {
+                                typeText(
+                                    { element: el_3 as HTMLElement, text: arrT[2], delay: 50 },
+                                    () => {
+                                        typeText(
+                                            { element: el_4 as HTMLElement, text: arrT[3], delay: 50 },
+                                            () => {
+                                                typeText(
+                                                    { element: el_5 as HTMLElement, text: arrT[4], delay: 50 },
+                                                    () => {
+                                                        typeText(
+                                                            { element: el_6 as HTMLElement, text: arrT[5], delay: 50 }
+
+                                                        )
+                                                    }
+                                                )
+                                            }
+                                        )
+                                    }
+                                )
+                            }
+                        )
                     }
                 )
             }, 2000)
@@ -52,7 +83,7 @@ function Landing() {
     }, [])
     return (
 
-        <div className='container'>
+        <div className='about'>
             <span>
                 <img src="/assets/profile.jpeg" alt="profile picture" />
                 <h2>Tran Quoc Huy</h2>
@@ -63,8 +94,22 @@ function Landing() {
                     <span></span>
                     <span className='blue'></span>
                 </h1>
-                <p>I'm currently studying Embedded Systems and IoT at <a className="blue" href="https://hcmute.edu.vn/" aria-label="HCMUTE website">HCMC University of Technology and Education</a></p>
-                <p>With my graduation anticipated by June 2024,<br /> my primary aspiration is to become a<span className='blue bold'>&#160;full-stack developer</span></p>
+                <p>
+                    <span>
+
+                    </span>
+                    <a className="blue" href="https://hcmute.edu.vn/" aria-label="HCMUTE website">
+
+                    </a>
+                </p>
+                <p>
+                    <span>
+
+                    </span>
+                    <span className='blue bold'>
+
+                    </span>
+                </p>
             </span>
         </div>
     )
