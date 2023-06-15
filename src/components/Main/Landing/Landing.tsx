57fdfd6594672b51ffb4abbbbf20b912c7644350
huy <tranquochuy645@gmail.com>
2023-06-15 21:04:01 +0700

    acp

--- src/components/Main/Landing/Landing.tsx
+++ src/components/Main/Landing/Landing.tsx
@@ -6,80 +6,62 @@ interface TypeTextOptions {
     delay: number;
 }
 function Landing() {
-    const arrT = [
-        "Hi, I'm ",
-        "Huy",
-        "I will be graduating in June 2024 with a major in Embedded Systems and IoT from ",
-        "HCMC University of Technology and Education",
-        "My main goal is to pursue a career as a ",
-        "full-stack developer"
-    ]
-
-
-    function typeText({ element, text, delay }: TypeTextOptions, callback?: () => void): void {
-        let index = 0;
-        const textContainer = document.createElement('span');
-        element.appendChild(textContainer);
-        const cursor = document.createElement('span');
-        cursor.className = 'cursor';
-        element.appendChild(cursor);
-        function typeNextLetter(): void {
-            if (index < text.length) {
-                textContainer.textContent += text.charAt(index);
-                index++;
-                setTimeout(typeNextLetter, delay);
-            } else {
-                element.removeChild(cursor);
-                if (callback) {
-                    callback();
+    function typeParaGraph(e: Array<HTMLElement>, t: Array<string>, delay: number) {
+        function typeText({ element, text, delay }: TypeTextOptions): Promise<void> {
+            return new Promise((resolve) => {
+                let index = 0;
+                const textContainer = document.createElement('span');
+                element.appendChild(textContainer);
+                const cursor = document.createElement('span');
+                cursor.className = 'cursor';
+                element.appendChild(cursor);
+                function typeNextLetter(): void {
+                    if (index < text.length) {
+                        textContainer.textContent += text.charAt(index);
+                        index++;
+                        setTimeout(typeNextLetter, delay);
+                    } else {
+                        element.removeChild(cursor);
+                        resolve();
+                    }
                 }
-            }
+
+                typeNextLetter();
+            });
         }
-        typeNextLetter();
+
+        function recurseWithIndex() {
+            typeText({ element: e[i], text: t[i], delay: delay }).then(
+                () => {
+                    if (i < t.length) {
+                        i++;
+                        recurseWithIndex();
+                    }
+                }
+            )
+        }
+
+        let i = 0
+        recurseWithIndex();
     }
 
 
     useEffect(() => {
-        const el_wrapper = document.querySelector('#landing>.about>span:last-of-type');
-        const el_1 = el_wrapper?.querySelector('h1>span:first-of-type');
-        const el_2 = el_wrapper?.querySelector('h1>span:last-of-type');
-        const el_3 = el_wrapper?.querySelector('p:first-of-type>span:first-of-type');
-        const el_4 = el_wrapper?.querySelector('p:first-of-type>a:first-of-type');
-        const el_5 = el_wrapper?.querySelector('p:last-of-type>span:first-of-type');
-        const el_6 = el_wrapper?.querySelector('p:last-of-type>span:last-of-type');
-        if (el_1 && el_2 && el_3 && el_4 && el_5 && el_6) {
-            setTimeout(() => {
-                typeText(
-                    { element: el_1 as HTMLElement, text: arrT[0], delay: 200 },
-                    () => {
-                        typeText(
-                            { element: el_2 as HTMLElement, text: arrT[1], delay: 200 },
-                            () => {
-                                typeText(
-                                    { element: el_3 as HTMLElement, text: arrT[2], delay: 50 },
-                                    () => {
-                                        typeText(
-                                            { element: el_4 as HTMLElement, text: arrT[3], delay: 50 },
-                                            () => {
-                                                typeText(
-                                                    { element: el_5 as HTMLElement, text: arrT[4], delay: 50 },
-                                                    () => {
-                                                        typeText(
-                                                            { element: el_6 as HTMLElement, text: arrT[5], delay: 50 }
-
-                                                        )
-                                                    }
-                                                )
-                                            }
-                                        )
-                                    }
-                                )
-                            }
-                        )
-                    }
-                )
-            }, 2000)
+        const arrT = [
+            "Hi, I'm ",
+            "Huy",
+            "I will be graduating in June 2024 with a major in Embedded Systems and IoT from ",
+            "HCMC University of Technology and Education",
+            "My main goal is to pursue a career as a ",
+            "full-stack developer"
+        ]
+        const el_wrapper = document.querySelector('#about-span-2');
+        let el: Array<any> = []
+        for (let i = 0; i < 6; i++) {
+            el.push(el_wrapper?.querySelector(`#t_${i}`));
         }
+        setTimeout(() => { typeParaGraph(el, arrT, 50) }, 2000);
+
     }, [])
     return (
 
@@ -91,22 +73,22 @@ function Landing() {
             </span>
             <span id="about-span-2" className='about-span'>
                 <h1 className="font-1" >
-                    <span></span>
-                    <span className='blue'></span>
+                    <span id='t_0'></span>
+                    <span id='t_1' className='blue'></span>
                 </h1>
                 <p>
-                    <span>
+                    <span id='t_2'>
 
                     </span>
-                    <a className="blue" href="https://hcmute.edu.vn/" aria-label="HCMUTE website">
+                    <a id='t_3' className="blue" href="https://hcmute.edu.vn/" aria-label="HCMUTE website">
 
                     </a>
                 </p>
                 <p>
-                    <span>
+                    <span id='t_4'>
 
                     </span>
-                    <span className='blue bold'>
+                    <span id='t_5' className='blue bold'>
 
                     </span>
                 </p>
