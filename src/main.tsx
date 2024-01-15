5d3e92abc35932d04fcb2f68fcfd36d9f4a87335
tranquochuy645 <tranquochuy645@gmail.com>
2024-01-15 17:31:39 +0700

    fix scroll behavior

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './keyframes.css'
 import BackGround from'./components/BackGround/BackGround.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
